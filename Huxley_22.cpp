8dc17a518b558a1cca8df19726145850bfaa2647
TETEURYAN <91018438+TETEURYAN@users.noreply.github.com>
2022-08-22 17:04:22 -0300

    22 - Mutiplication table

---
+++ Huxley_22.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+//22 - TABUADA
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int x,c1,c2,c3,c4,c5,c6,c7,c8,c9;
+    scanf("%i", &x);
+    
+    c1 = x*1; c2 = x*2;c3 = x*3; c4 = x*4;
+    c5 = x*5; c6 = x*6;c7 = x*7; c8 = x*8;
+    c9 = x*9;
+  
+    printf("%i X 1 = %i\n",x,c1);
+    printf("%i X 2 = %i\n",x,c2);
+    printf("%i X 3 = %i\n",x,c3);
+    printf("%i X 4 = %i\n",x,c4);
+    printf("%i X 5 = %i\n",x,c5);
+    printf("%i X 6 = %i\n",x,c6);
+    printf("%i X 7 = %i\n",x,c7);
+    printf("%i X 8 = %i\n",x,c8);
+    printf("%i X 9 = %i\n",x,c9);
+    
+    return 0;
+}
+    
