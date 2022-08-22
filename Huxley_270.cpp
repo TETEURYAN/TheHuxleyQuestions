f6f2a51664a56a176982c5442589f3eec70f109c
TETEURYAN <91018438+TETEURYAN@users.noreply.github.com>
2022-08-22 14:21:55 -0300

    270 - Extremely Basic
    Find the value of x.

---
+++ Huxley_270.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+//270 - EXTREMAMENTE BÁSICO.
+int main ()
+{
+	int A,B,X;
+	scanf("%i", &A);
+	scanf("%i", &B);
+	X = A + B;
+	printf("X = %i", X);
+	return 0;
+}
