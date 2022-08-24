73a9df2fb0c2b0c9b4001a9a9496c484e0f49158
TETEURYAN <91018438+TETEURYAN@users.noreply.github.com>
2022-08-23 22:03:50 -0300

    6 - Bhaskara formula
    Bhaskara formula in C to The Huxley.

---
+++ Huxley_6.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+//6 - EQUAÇÃO DO 2º GRAU
+int main ()
+{
+	double a,b,c,d,x1,x2;
+	
+	scanf("%lf", &a); 
+	scanf("%lf", &b); 
+	scanf("%lf", &c);
+	
+	d=b*b-4*a*c;
+	
+	 if( a == 0){
+        printf("NEEQSG");
+        system("pause");
+	}
+     if( d<0){
+        printf("NRR");   
+    } else {
+        x1 = (-b + sqrt(d)) / (2 * a);
+        x2 = (-b - sqrt(d)) / (2 * a);
+        printf("%.2lf\n", x1);
+        printf("%.2lf", x2);
+   }
+   return 0;
+}
+
+
