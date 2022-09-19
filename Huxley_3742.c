fb8d18e265efe1756600367f108a4eed7277ab97
TETEURYAN <91018438+TETEURYAN@users.noreply.github.com>
2022-09-18 22:15:42 -0300

    3742 - Fahrenheit to Celsius
    Fahrenheit to Celsius in C to The Huxley

---
+++ Huxley_3742.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+//3121 - TAMANHO DA CAVERNA
+
+int main(){
+	
+	float fahrenheit, celsius;
+	
+	scanf("%f", &fahrenheit);
+	
+	printf("%.10f", 5*(fahrenheit - 32)/9);
+	
+	return 0;
+	
+}
