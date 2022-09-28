425617f3e176779c4edbfa81c0e252371a9cd650
TETEURYAN <91018438+TETEURYAN@users.noreply.github.com>
2022-09-28 00:30:22 -0300

    3281 - Reverse order
    Reverse order in C to The Huxley

---
+++ Huxley_3281.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+//3281 - ORDEM INVERSA
+int main() {
+     int a,b;
+     
+     scanf("%i%i", &a, &b);
+     
+     printf("%i\n%i", b, a);
+    
+	return 0;
+}
