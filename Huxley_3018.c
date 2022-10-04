1ee50978d0ef3ed2e0a42ef31def12294596d436
TETEURYAN <91018438+TETEURYAN@users.noreply.github.com>
2022-10-04 00:37:44 -0300

    3018 - Enchendo o tanque
    Enchendo o tanque em C para o The Huxley

---
+++ Huxley_3018.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+// 3018 - ENHENDO O TANQUE
+int main()
+{double valor, nota;scanf("%lf %lf", &valor, &nota);printf("Você conseguiu colocar %.2lf litros!\n", nota/valor);return 0;}
