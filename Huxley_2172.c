3911f547bef6714bc514620753a6b515a7c992b3
TETEURYAN <91018438+TETEURYAN@users.noreply.github.com>
2022-10-10 00:14:25 -0300

    2172 - Run of the moles
    Run of the moles in C to The Huxley

---
+++ Huxley_2172.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+//2172 - Run of the moles
+int total(int qtd, int pata)
+{
+    if (pata > qtd){
+        return 0;
+    }
+    else{
+        int tunel;
+        scanf("%d", &tunel);
+        if (tunel == -1)
+        {
+            return (pata * 2) + total(qtd, pata+1);
+        }
+        else
+        {
+            return total(qtd, pata+1);
+        }
+    }
+}
+int main()
+{
+    int qtd;
+    scanf("%d", &qtd);
+    printf("%d\n", total(qtd, 1));
+    return 0;
+} 
