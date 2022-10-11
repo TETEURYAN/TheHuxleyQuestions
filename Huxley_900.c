923f462d17aa6bc12cb9e2afeeb3dbc2362a4048
TETEURYAN <91018438+TETEURYAN@users.noreply.github.com>
2022-10-11 00:09:07 -0300

    900 - Media of Numbers
    Media of Numbers in C to The Huxley.

---
+++ Huxley_900.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+//900 - Media of Numbers
+void repete(int entrada, double num, double soma){
+  
+    if(entrada == 0){
+        printf("Media dos numeros: %.15lf", soma/100);
+        return 0;
+    }
+    else{
+        printf("Digite um numero:\n");
+        scanf("%lf", &num);
+        soma += num;
+        //printf("%lf", soma);
+        repete(entrada-1, num, soma);
+    }
+    
+}
+int main() {
+    int num;
+    repete(100, num, 0);
+	return 0;
+}
