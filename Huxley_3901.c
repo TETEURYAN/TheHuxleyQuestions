55642cc2312316045485d511638b23b3d4fd60e6
TETEURYAN <91018438+TETEURYAN@users.noreply.github.com>
2022-10-19 21:08:27 -0300

    3901 - Armague 2
    Armague 2 in C to The Huxley.

--- Huxley_3901.c
+++ Huxley_3901.c
@@ -1,79 +1,70 @@
 #include<stdio.h>
+#include<stdbool.h>
+//3901 - Armague 2
 
-int mdc(int n1,int n2){
-    if(n2==0) return n1;
-    return mdc(n2,n1%n2);
+bool mdc (int vidaA, int vidaB) {
+    if (vidaA % vidaB == 0) {
+        if (vidaB == 1) return true;
+        else return false;
+    }
+    mdc(vidaB, vidaA % vidaB);
 }
 
-int funcPrimo(int a, int i)
+bool funcPrimo (int nTeste, int divisor) {
+    if (nTeste == 2 || divisor == 1) return true;
+    else if (nTeste % 2 == 0 || nTeste % divisor == 0 || nTeste < 2) return false;
+
+    funcPrimo(nTeste, divisor - 2);
+}
+
+int habilidade (int habilidade, int atk1, int vd1, int vd2, int round) 
 {
-  if(a < 2) return 0;
-  if (a == 2) {
-    return 1;
-  }
-  if (a % i == 0) return 0;
-  if (i * i > a)
-    return 1;
-  return funcPrimo(a,i+1);
+    if(habilidade == 1){
+        if(funcPrimo(round, round-2)) atk1 *= 2;    
+    }
+    if(habilidade == 2){
+        if(mdc(vd1, vd2)) atk1 += 25;
+    }
+    if(habilidade == 3){
+        if(funcPrimo(vd1, vd1-2)) atk1 += 10;
+        else if(funcPrimo(atk1, atk1 - 2)) atk1 += 10;
+    }
+    return atk1;
 }
 
+bool confronto (int habilidade1,int atk1,int vd1,int habilidade2,int atk2,int vd2, int round) {
+    atk1 = habilidade(habilidade1, atk1, vd1, vd2, round);
+    vd2 -= atk1; 
 
-int partida(int rodada, int k, int i){
+    if (vd2 <= 0) return true;
+    atk2 = habilidade(habilidade2, atk2, vd2, vd1, round);
+    vd1 -= atk2;
+
+    if (vd1 <= 0) return false;
+    if (habilidade1 == 1 && funcPrimo(round, round-2)) atk1 /= 2;
+    if (habilidade2 == 1 && funcPrimo(round, round-2)) atk2 /= 2;
+    
+    confronto(habilidade1, atk1, vd1, habilidade2, atk2, vd2, round + 1);
+}
+
+int partida(int rodada, int Pts_will, int Pts_gabriel){
 	int primeiro, habilidade_will,atk_will, vd_will, habilidade_gabriel, atk_gabriel, vd_gabriel;
 	
 	if(rodada == 0){
-        printf("Will venceu %d rodadas\nGabriel venceu %d rodadas", k, i); 
+        printf("Will ganhou %d rodadas\nGabriel ganhou %d rodadas", Pts_will, Pts_gabriel); 
         return 0;
     }
 	else{
 		scanf("%i%i%i%i%i%i%i", &primeiro, &habilidade_will, &atk_will, &vd_will, &habilidade_gabriel, &atk_gabriel, &vd_gabriel);
-		
-			//habilidade do will
-			if(habilidade_will == 1){
-				atk_will *= (funcPrimo(rodada,2)) ? 2 : 1;
-			}
-			if(habilidade_will == 2){
-				atk_will += (mdc(vd_will,vd_gabriel) == 1) ? 25 : 0;
-			}
-			if(habilidade_will == 3){
-				atk_will += ((funcPrimo(atk_will,2) || funcPrimo(vd_will,2))) ? 10 : 0;	
-			}
-			//habilidade do gabriel
-			if(habilidade_gabriel == 1){
-				atk_gabriel *= (funcPrimo(rodada,2)) ? 2 : 1;
-			}
-			if(habilidade_gabriel == 2){
-				atk_gabriel += (mdc(vd_gabriel,vd_will) == 1) ? 25 : 0;
-			}
-			if(habilidade_gabriel == 3){
-				atk_gabriel += ((funcPrimo(atk_gabriel,2) || funcPrimo(vd_gabriel,2))) ? 10 : 0;	
-			}
-			//duelo sendo k = will e i = Gabriel
-			 if(primeiro == 1){
-			 	if((vd_gabriel/atk_will)<(vd_will/atk_gabriel)){
-			 		k++;
-				 }
-				 else{
-				 	i++;
-				 }
-			 }
-			 
-			 if(primeiro == 2){
-			 	if((vd_will/atk_gabriel)<(vd_gabriel/atk_will)){
-			 		i++;
-				 }
-				 else{
-				 	k++;
-				 }
-			 }
-			 
-		
+        	
+			 if(primeiro == 1) confronto(habilidade_will,atk_will, vd_will, habilidade_gabriel, atk_gabriel, vd_gabriel,1) ? Pts_will++ : Pts_gabriel++; 
+			 if(primeiro == 2) !confronto(habilidade_gabriel,atk_gabriel, vd_gabriel, habilidade_will, atk_will, vd_will,1) ? Pts_will++ : Pts_gabriel++;	
 	}
-	return partida(rodada-1, k, i);
+	partida(rodada-1, Pts_will, Pts_gabriel);
 }
 
 int main(){
-	int rodadas, j, i;
+	int rodadas;
 	scanf("%i", &rodadas);
 	
 	partida(rodadas, 0, 0);
