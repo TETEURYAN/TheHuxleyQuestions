ddf310f6ab4dca1dd5acf51c09d8a348c37286ab
TETEURYAN <91018438+TETEURYAN@users.noreply.github.com>
2022-10-25 20:30:52 -0300

    180 - Elevator
    Elevator in C to The Huxley.

---
+++ Huxley_180.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include<stdbool.h>
+//180 - Elevator
+int passou(int entrada, int capacidade, int i, int agora)
+{
+    if (i > entrada)return false;
+    
+    else
+    {
+        int saiu, entrou, nagora;
+        scanf("%d%d", &saiu, &entrou);
+        nagora = (agora + entrou) - saiu;
+        if (agora > capacidade)return true;
+        return passou(entrada, capacidade, i+1, nagora);
+    }
+}
+int main()
+{
+    int entrada, capacidade;
+    scanf("%d%d", &entrada, &capacidade);
+ 
+    printf("%s", passou(entrada, capacidade, 1, 0) ? "S\n" : "N\n" );
+    return 0;
+}
