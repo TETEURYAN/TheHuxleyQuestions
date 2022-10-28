55d57c21fb3abde6fac780e0f1e938a84183eb9a
TETEURYAN <91018438+TETEURYAN@users.noreply.github.com>
2022-10-27 23:37:49 -0300

    3771 - Trip time
    Trip time in C to The Huxley.

--- Huxley_3771.c
+++ Huxley_3771.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <math.h>
-//3771 - Tempo de voagem
+//3771 - Tempo de viagem
+//https://www.thehuxley.com/problem/3771/code-editor/
 
 double distanciaEntreDoisPontos(double xa, double ya, double xb, double yb) {
     return sqrt(pow(xb - xa, 2.0) + pow(yb - ya, 2.0));
@@ -10,15 +11,9 @@ int main(){
 	int x1, y1, x2, y2, veloMilos;
 	double dist, tempo;
 	
-	scanf("%i%i%i%i%i", &x1, &y1, &x2, &y2, &veloMilos);
-	
-	dist = distanciaEntreDoisPontos(x1,y1,x2,y2);
-	
+	scanf("%i%i%i%i%i", &x1, &y1, &x2, &y2, &veloMilos);	
+	dist = distanciaEntreDoisPontos(x1,y1,x2,y2);	
 	tempo = dist/veloMilos;
-	
 	printf("Milos esta no ponto (%i, %i) indo para (%i, %i) e provavelmente ira chegar em %.2lf horas.\n", x1,y1,x2,y2,tempo);
-	
-	
-	
-	
+
 }
