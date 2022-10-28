90656181ee6326dfa496471a7167359c47da12f4
TETEURYAN <91018438+TETEURYAN@users.noreply.github.com>
2022-10-27 23:00:39 -0300

    2845 - books in the backpack
    Books in the backpack in C to The Huxley.

---
+++ Huxley_2845.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+//2845 - books in the backpack
+void peso(int soma, int c)
+{
+    int carga;
+    scanf("%d", &carga);
+    soma += carga;c++;
+    if(soma>=18){printf("\nPodem ser carregados %d livros.", c-1);return;}
+    peso(soma,c);
+}
+
+int main() 
+{
+    int i;
+    printf("Insira o peso dos livros:");
+    peso(0,0);
+	return 0;
+}
