fd28f15b3e6195a420c85191847fb047dfa83664
TETEURYAN <91018438+TETEURYAN@users.noreply.github.com>
2022-10-27 23:35:48 -0300

    378 - World cup
    World cup in C to The Huxley.

---
+++ Huxley_378.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+//378 - world cup
+//https://www.thehuxley.com/problem/378
+int main(){
+  int a1, b1, c1, d1, e1, f1, t1;
+  
+  scanf ("%i %i %i %i %i %i",&a1,&b1,&c1,&d1,&e1,&f1);
+  
+  t1 = a1+b1+c1+d1+e1+f1;
+  
+  if (t1>100){
+    printf("Classificado");}
+  else {
+    printf("Eliminado");}
+    
+return 0;}
