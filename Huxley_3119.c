3abe14ff0b94a53753aba725a5f470cf17b1ab19
TETEURYAN <91018438+TETEURYAN@users.noreply.github.com>
2022-10-27 23:22:40 -0300

    3119 - Nickname
    Nickaname in C to The Huxley.

---
+++ Huxley_3119.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+//3119 - Nickname
+int main() {
+    char name1[10];
+    char name2[10];
+    
+    scanf("%s%s", &name1, &name2);
+    
+    printf("%s%s", name1, name2);
+	return 0;
+}
