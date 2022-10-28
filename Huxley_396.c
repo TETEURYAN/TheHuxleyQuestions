022cae67f76c48335a1e418ef4e0ffc86d947f00
TETEURYAN <91018438+TETEURYAN@users.noreply.github.com>
2022-10-27 23:58:07 -0300

    396 - Down with the pollution
    Down with the pollution in C to The Huxley.

---
+++ Huxley_396.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+//396 -Down with the pollution
+//https://www.thehuxley.com/problem/396/code-editor/
+double loop(int c, int j)
+{
+    int num;
+    scanf("%i", &num);
+    if(num == 999){ printf("%.2lf\n%d", j*12.89, c); return 0;}
+    else{
+        if(num >2){ c++; j += num-2;}
+    }
+    return loop(c,j);
+}
+
+int main() {
+    loop(0,0);
+	return 0;
+}
