3c4d1e11c778f9eb404a50a1abdecac5e425f23f
TETEURYAN <91018438+TETEURYAN@users.noreply.github.com>
2022-10-30 22:56:22 -0300

    3731 - Dorian's Library
    Dorian's Library in C to The Huxley.

--- Huxley_3731.c
+++ Huxley_3731.c
@@ -1,51 +1,121 @@
 #include <stdio.h>
-#include <string.h>
 #include <math.h>
-#include <stdlib.h>
+#include <stdbool.h>
 
-//3731 - Dorian's library
+#define SOMA 0
+//3731 - A bilbioteca de Dorian
+//https://thehuxley.com/problem/3731
 
-int proxprimo(int x, int y) 
-{
-    if(x < 2 ||x % y == 0) x++;
-    else if(x == 2 || y*y > x ) return x;
-    return proxprimo(x, y+1);
+//funções para calculo e array
+int recebendoValores (int codigos[], int posicoes[], int i) {
+    scanf("%d %d", &codigos[i], &posicoes[i]);
+    if (!codigos[i] && !posicoes[i]) return i - 1;
+    recebendoValores(codigos, posicoes, i + 1);
 }
 
+int somaDoDenominador (int num, int soma) {
+    if (num < 10) return soma + num;
+    somaDoDenominador(num / 10, soma + (num % 10));
+}
+
+int primo(int n, int div){
+    if(n < 2) return 0;
+    if(div > sqrt(n)) return 1;
+    if(n % div == 0) return 0;
+    else primo(n, div+1); 
+}
+
+int proxPrimo(int n){
+    if(primo(n, 2)) return n;
+    else return proxPrimo(n+1);
+}
+
+int verificandoParidade (int num) {
+    if (num == 0 || num == 1) return 2;
+    else if (num % 2 == 0) num += 1;
+    else num += 2;
+    return proxPrimo(num);
+}
+ 
+int somaDoNumerador (int codigo) {
+    return codigo + verificandoParidade(codigo);
+}
 int somadigito(int num, int soma){
     if(num==0)return soma;
     else soma += num%10;
     somadigito(num/10,soma);
 }
 
-int somamultiplos( int num1, int i, int soma)
+int somaDosMultiplos (int nCodigo, int soma, int i) {
+    if (i * 6 >= nCodigo) return soma;
+    soma += (i * 6);
+    somaDosMultiplos(nCodigo, soma, i + 1);
+}
+
+int somamultiplos( int codigos, int i, int soma)
 {
-    if(i == num1) return soma;
+    if(i == codigos) return soma;
     else if(i % 6 == 0) soma+= i;
-    somamultiplos(num1, i+1, soma);
+    somamultiplos(codigos, i+1, soma);
 }
 
-int primeiro_numero(int num1)
+int primeiro_numero(int codigos)
 {
-    num1 = (num1 + proxprimo(num1,2))/somadigito(num1,0); return num1;
+    codigos = (codigos + proxPrimo(codigos))/somadigito(codigos,0); return codigos;
 }
 
-int segundo_numero(int num1, int num2)
+int segundo_numero(int codigos, int posicoes)
 {
-    num2 = abs(somamultiplos(num1,0,0) - num2); return num2;
+    posicoes = fabs(somamultiplos(codigos,0,0) - posicoes); return posicoes;
 }
- int entrada( )
-{
-    int num1, num2;
-    scanf("%i%i", &num1, &num2);
-    if(num1 == 0 && num2 == 0) return 0;
-    else{
-        printf("%i - %i\n", primeiro_numero(num1), segundo_numero(num1,num2));
-    }    
-     entrada();
+//funções para novos valoroes
+int novoCodigo (int codigo) {
+    return somaDoNumerador(codigo)/somadigito(codigo,0);
 }
 
-void main(){
-	
-	entrada();
+int novaPosicao (int posicao, int codigo) {
+    return somaDosMultiplos(codigo, SOMA, 1) - posicao;
 }
+
+void New_value (int codigos[], int posicoes[], int i) {
+    if (i < 0) return;                      // i = indices usados
+
+    posicoes[i] = fabs(novaPosicao(posicoes[i], codigos[i]));
+    codigos[i] = novoCodigo(codigos[i]);
+    New_value(codigos, posicoes, i - 1);
+}
+
+void Valores_anteriores (int valores[], int i, int j, int max) {
+    if (j > max) return;
+
+    if (valores[i] == valores[j]) {
+        valores[j] += 1;
+    }
+    Valores_anteriores(valores, i, j + 1, max);
+}
+
+void valores_repetidos (int valores[], int i, int max) {
+    if (i == max) return;
+    Valores_anteriores(valores, i, i + 1, max);
+    valores_repetidos(valores, i + 1, max);
+}
+
+void printandoNew_value (int codigos[], int posicoes[], int i) {
+    if (i < 0) return;
+    printf("%d - %d\n", codigos[i], posicoes[i]);
+    printandoNew_value(codigos, posicoes, i - 1);
+}
+
+int main () {
+    int codigos[1000], posicoes[1000], i = 0;
+    i = recebendoValores(codigos, posicoes, i);
+
+    New_value(codigos, posicoes, i);
+    
+    valores_repetidos(codigos, 0, i);
+    valores_repetidos(posicoes, 0, i);
+
+    printandoNew_value(codigos, posicoes, i);
+    return 0;
+}
+//Parte do código usei as funções do meu grande amigo Caio César
