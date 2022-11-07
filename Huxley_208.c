9e23fdf7b9272ff5dd82fc51258344022a7cae85
TETEURYAN <91018438+TETEURYAN@users.noreply.github.com>
2022-11-07 01:14:38 -0300

    208 - Selos
    Selos in C to The Huxley.

---
+++ Huxley_208.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+//208 - Selos
+//https://thehuxley.com/problem/208
+int main()
+{
+  long long int selos; int razao;
+  
+  scanf("%Ld\n", &selos);
+
+  for (razao = 2; razao < sqrt(selos); razao ++)
+  {
+    if (selos % razao == 0)printf("S\n");return 0;
+  }
+  printf("N\n");
+  return(0);
+}
