effc81fe9e29663cfbdef38c786c66241fb015bd
TETEURYAN <91018438+TETEURYAN@users.noreply.github.com>
2022-11-06 23:21:04 -0300

    446 - Toothbrush
    Toothbrush in C to The Huxley.

---
+++ Huxley_446.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+//446 - Toothbrush
+//https://thehuxley.com/problem/446
+int main() {
+    int num;
+    scanf("%d", &num);
+    printf("%.2f", ((float)num - (num/3)) * 2.2);
+	return 0;
+}
