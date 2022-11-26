1d7cb47247c46f9dd453dd0881120653f90eec7e
TETEURYAN <matheusryan61@gmail.com>
2022-11-26 13:08:47 -0300

    2738 - Mineirin vs Cuabo I

--- Huxley_2378.c
+++ Huxley_2378.c
@@ -9,11 +9,12 @@ void printa_array(int array[], int i, int tam){
     printa_array(array, i+1, tam);
 }
 
-int indice(int array[], int tamanho, int value)
+void input_array(int array[], int aux[], int i, int tam)
 {
-    if(tamanho == 0) return -1;
-    if(array[tamanho] == value) return tamanho;
-    return indice(array, tamanho - 1, value);
+  if (i == tam)  return; 
+  scanf("%d", &array[i]); 
+  aux[i] = array[i];
+  input_array(array, aux, i+1, tam);
 }
 
 void swap(int* a, int* b)
@@ -23,24 +24,31 @@ void swap(int* a, int* b)
     *b = aux;
 }
 
-void bubble_sort ( int array[], int n)
+void bubble_sort (int i, int j, int array[], int n)
 {
-	for( int i = 0; i<n; i++)
+	int aux;
+	if (i < n)
 	{
-	    for( int j = 0; j<(n-1);j++)
-	    {
-	        if(array[j]>array[j+1])
-	        {
-	            
-	            int temp = array[j];
-	            array[j] = array[j+1];
-	            array[j+1] = temp;
-	        }
-	    }
+		if (j < n)
+		{
+			if (n != j + 1)
+				if (array[j] > array[j + 1])
+					swap(&array[j], &array[j + 1]);
+				
+			bubble_sort(i, j + 1, array, n);
+		}
+		else
+		{
+			bubble_sort(i, i, array, n - 1);
+		}
 	}
-	    printa_array(array, 0, 4);
-	    //printf("\n%d\n", c);
-	
+}
+
+int indice(int array[], int tamanho, int value)
+{
+    if(tamanho == 0) return -1;
+    if(array[tamanho] == value) return tamanho;
+    return indice(array, tamanho - 1, value);
 }
 
 void vezes( int i, int array[], int aux[], int c)
@@ -57,27 +65,16 @@ void vezes( int i, int array[], int aux[], int c)
 	else printf("\n%d", c); return;
 }
 
-
-void input_array(int array[], int aux[], int i, int tam)
-{
-  if (i == tam)  return; 
-  scanf("%d", &array[i]); 
-  aux[i] = array[i];
-  input_array(array, aux, i+1, tam);
-}
-
-
-
 int main ()
 {
 
 	
 	int array[4]; int aux[4];
 	input_array(array,aux, 0, 4);
-	bubble_sort(array, 4);
+	bubble_sort(0,0,array, 4);
+	
+	printa_array(array,0,4);
 	vezes(0,array, aux,0);
 
-	
-	
 	return 0;
 }
