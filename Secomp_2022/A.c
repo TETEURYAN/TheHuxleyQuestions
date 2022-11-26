c9ff66243488c2c05499ee2d1c0330374e573d5d
TETEURYAN <matheusryan61@gmail.com>
2022-11-26 10:54:02 -0300

    Dividindo chocolate

---
+++ Secomp_2022/A.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+int main()
+{
+    int num1, num2;
+
+    scanf("%d%d", &num1, &num2);
+
+    
+    printf ("%d\n", num1/num2);
+    return 0;
+}
