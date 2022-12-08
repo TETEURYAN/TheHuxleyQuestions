95cde2e753a6b25dadbc74abd1c83b34969df3eb
TETEURYAN <matheusryan61@gmail.com>
2022-12-07 21:35:08 -0300

    4120 - Ajuda vendedores

---
+++ Huxley_4120.c
@@ -0,0 +1,11 @@
+//4120 - Ajuda vendedores
+// https://thehuxley.com/problem/4120
+
+valor = float(input())
+
+
+print('A vista com desconto de 10%: {:.2f}'.format(valor*0.90))
+print('Valor da parcela em 3x sem juros: {:.2f}' .format(valor*1/3))
+print('Comissao do vendedor a vista: {:.2f}'.format(valor*0.9*0.05))
+print('Comissao do vendedor a prazo: {:.2f}'.format(valor*0.05))
+
