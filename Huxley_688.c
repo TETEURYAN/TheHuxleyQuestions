297f9494b31440e2246e8d29fb3f2fa2e8c18dd2
TETEURYAN <91018438+TETEURYAN@users.noreply.github.com>
2022-12-11 23:55:02 -0300

    688 - Existe solução?

---
+++ Huxley_688.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int mdc(int n1,int n2){
+    if(n2==0) return n1;
+    return mdc(n2,n1%n2);
+}
+
+int main() 
+{
+    int a, b, c;
+    
+    scanf("%d%d%d", &a, &b, &c);
+    
+    printf("%c\n", !(c % mdc(a,b)) ? 'S' :'N');
+	return 0;
+}
