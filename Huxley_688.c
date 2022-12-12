e96a4bcc994b14076c099d54684bd32df7bd39b1
TETEURYAN <91018438+TETEURYAN@users.noreply.github.com>
2022-12-11 23:55:50 -0300

    688 - Existe solução?
    Existe solução? em C para o The Huxley.

--- Huxley_688.c
+++ Huxley_688.c
@@ -3,6 +3,9 @@
 #include <math.h>
 #include <stdlib.h>
 
+//688 - Existe solução?
+//https://thehuxley.com/problem/688
+
 int mdc(int n1,int n2){
     if(n2==0) return n1;
     return mdc(n2,n1%n2);
