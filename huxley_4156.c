a37dc24fa3f9f5a3423017cb9b14e8cbc898f169
TETEURYAN <91018438+TETEURYAN@users.noreply.github.com>
2022-12-17 23:57:08 -0300

    4156 - Xadrez no IC

--- huxley_4156.c
+++ huxley_4156.c
@@ -125,3 +125,113 @@ int main(){
 
     return 0;
 }
+
+
+#include <stdio.h>
+ 
+// campo é o tabuleiro e m é a matriz de ataque de Pedro, x e y é a posicao da peca atacante
+void ataque_torre(int campo[8][8], int m[8][8], int x, int y)
+{
+    //ataca pra a direita
+    for(int i = x;i < 8; i++)
+    {
+        if(campo[i][y] < 3 && campo[i][y] > 0 && i != x)
+            break;
+        m[i][y] = 1;
+    }
+    //ataca pra a esquerda
+    for(int i = x;i >= 0; i--)
+    {
+        if(campo[i][y] < 3 && campo[i][y] > 0 && i != x)
+            break;
+        m[i][y] = 1;
+    }
+    //ataca pra baixo
+    for(int i = y;i < 8; i++)
+    {
+        if(campo[x][i] < 3 && campo[x][i] > 0 && y != i)
+            break;
+        m[x][i] = 1;
+    }
+    //ataca pra cima
+    for(int i = y;i >= 0; i--)
+    {
+        if(campo[x][i] < 3 && campo[x][i] > 0 && y != i)
+            break;
+        m[x][i] = 1;
+    }
+ 
+}
+void ataque_rei(int campo[8][8], int m[8][8], int x, int y)
+{
+    //ataca todas as casas em todas as direcoes em range 1
+    for(int i = x - 1; i <= x + 1; i++)
+        for(int j = y - 1; j <= y + 1; j++)
+            if(i >= 0 && i < 8 && j >= 0 && j < 8)
+                m[i][j] = 1;
+}
+ 
+int main()
+{
+/*
+    Explicacao do algoritmo, pra cada peça atacante, guardo todas as casas sendo atacadas em uma matriz
+    com isso, checaremos se todas as casas em volta do rei estao sendo atacadas
+*/
+ 
+    int campo[8][8], atacadas[8][8] = {0}, rei_x, rei_y, afogamento = 0, cheque = 0;
+    
+// scanea o campo
+    for(int i = 0; i < 8; i++)
+        for(int j = 0; j < 8; j++)
+            scanf("%d", &campo[i][j]);
+ 
+// faz os ataques, registra na matriz de atacados, e acha o rei
+    for(int i = 0; i < 8; i++)
+        for(int j = 0; j < 8; j++)
+        {
+            if(campo[i][j] == 1)
+                ataque_torre(campo, atacadas, i, j);
+ 
+            else if(campo[i][j] == 2)
+                ataque_rei(campo, atacadas, i, j);
+ 
+            else if(campo[i][j] == 3)
+            {
+                rei_x = i;
+                rei_y = j;
+            }
+        }
+ 
+// conta quantas casas estão sendo atacadas no quadrado 3x3 em volta do rei
+    for(int i = rei_x - 1; i <= rei_x + 1; i++)
+        for(int j = rei_y - 1; j <= rei_y + 1; j++)
+            if(i < 0 || i >= 8 || j < 0 || j >= 8 || atacadas[i][j] == 1)
+                afogamento++;
+ 
+// checa se o rei esta em cheque
+    if(atacadas[rei_x][rei_y] == 1)
+        cheque = 1;
+ 
+// se 9 casas no quadrado 3x3 em volta do rei estiverem atacadas, xeque-mate
+    if(afogamento == 9)
+        printf("Xeque-mate\n");
+ 
+// se o rei nao esta em cheque mas nao pode se mover(todas as casas em torno dele estao atacadas), afogamento
+    else if(afogamento == 8 && cheque == 0)
+        printf("Afogado\n");
+ 
+// se o rei pode sair do ataque, nao ocorreu ainda
+ 
+    else
+        printf("Nao ocorreu ainda\n");
+ 
+// Descomente para ver a matriz de atacadas
+// for(int i = 0; i < 8; i++)
+// {
+//     for(int j = 0; j < 8; j++)
+//         printf("%d ", atacadas[i][j]);
+//     printf("\n");
+// }
+ 
+    return 0;
+}
