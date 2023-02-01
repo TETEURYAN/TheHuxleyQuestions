1011cac8e3f676dc058911e731a6874c084d08c8
TETEURYAN <91018438+TETEURYAN@users.noreply.github.com>
2023-01-31 23:58:06 -0300

    22 - TABUADA
    TABUADA em C para o The Huxley.

--- Huxley_22.cpp
+++ Huxley_22.cpp
@@ -2,11 +2,8 @@
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
+
 //22 - TABUADA
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
 
 int main() {
     int x,c1,c2,c3,c4,c5,c6,c7,c8,c9;
