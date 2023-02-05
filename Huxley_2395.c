7b05ffb16752345e6c0cabcdb165b22506b3fabe
TETEURYAN <91018438+TETEURYAN@users.noreply.github.com>
2023-02-04 23:21:50 -0300

    2395 - E/S CPF

---
+++ Huxley_2395.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+#include <iso646.h>
+#include <stdlib.h>
+
+void input_string(char *nickname, int i, int tam) 
+{
+    char c = getchar();
+    if(c == '\n' or i == tam) nickname[i] = '\0';
+    else 
+    {
+        nickname[i] = c;
+        input_string(nickname, i + 1, tam);
+    }   
+}
+
+int convert(char *string, int tam, int j)
+{
+    for(int i = j; i < tam; i++)
+        printf("%c", string[i]);
+    printf("\n");
+}
+
+void solve( char *cpf)
+{
+    convert(cpf, 3, 0);
+    convert(cpf, 7, 4);
+    convert(cpf, 11, 8);
+    convert(cpf, 14, 12);
+}
+
+int main() 
+{
+    char cpf[15];
+    input_string(cpf, 0, 15);
+    solve(cpf);
+    
+	return 0;
+}
