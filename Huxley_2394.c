caebd3b30e0c413d96c70309c1876fc3b5b7d82d
Matheus Ryan <mrysn@ic.ufal.br>
2023-05-01 23:57:45 -0300

    2394 - Pilha Antigo
    Pilha Antigo using stack methods in C to The Huxley

---
+++ Huxley_2394.c
@@ -0,0 +1,82 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<iso646.h>
+#include<stdbool.h>
+
+//2394 - Pilha Antigo
+//https://thehuxley.com/problem/2394
+
+#define MAX 10
+#define EMPTY -1
+
+typedef struct Pilha{
+    int topo;
+    int vetor[10];
+    
+}Pilha;
+
+int push (int value);
+int pop (void);
+int fullStack(void);
+int emptyStack(void);
+void printStack(void);
+
+Pilha stack;
+
+int main(){
+   int opcao,valor;
+   char code[12];
+   strcpy(code, "value");
+   stack.topo=-1;
+  
+    while(1){
+        scanf("%s", code);
+        if(not strcmp(code, "Empilhar")){
+            scanf("%d\n", &valor);
+            push(valor);
+        }
+        if(not strcmp(code, "Desempilhar")){
+            pop();
+        }
+        if(not strcmp(code, "Fim")){
+            exit(0);
+        }
+        printStack();
+    }    
+}   
+
+int push (int value){
+    stack.topo++;  
+    stack.vetor[stack.topo] = value; 
+    return stack.topo, stack.vetor[stack.topo];  
+}
+ 
+int pop (void){
+    
+    int auxvalor=stack.vetor[stack.topo];
+    stack.topo--;
+    return auxvalor;
+} 
+
+int fullStack(void){
+    
+    if(stack.topo<MAX)
+        return false;  
+    else
+        return true;
+}
+
+int emptyStack(void){
+    if(stack.topo == EMPTY)	
+        return true;
+   else
+        return false;
+}
+
+void printStack(void)
+{
+    for(int i = stack.topo; i > -1; i--)
+        printf("%d%c", stack.vetor[i],(i) ? ' ' : '\n');
+   
+}
