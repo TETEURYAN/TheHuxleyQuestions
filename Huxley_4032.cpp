1e20ff5ed28aee86c105f35eeb9e25bb8ed11b73
Matheus Ryan <mrysn@ic.ufal.br>
2024-05-05 22:25:46 -0300

    4032 - Nova Propriedade

---
+++ Huxley_4032.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+ 
+using namespace std;
+
+//4032 - Nova Propriedade
+// https://thehuxley.com/problem/4032
+
+const int N1 = (int)2e5 + 100;
+#define PI 3.14159265358979323846264338327
+#define forn for(int i=0;i<n;i++)
+#define fornn for(int i=1;i<=n;i++)
+#define each(i,c) for(__typeof((c).begin()) i=(c).begin(),i##_end=(c).end();i!=i##_end;++i)
+#define all(x) (x).begin(), (x).end()
+#define f first
+#define s second
+#define pb push_back
+#define pp pop_back
+#define mp make_pair
+#define yes cout<<"Yes"<<'\n';
+#define no  cout<<"No"<<'\n';
+#define one  cout<<-1<<'\n';
+#define nl  '\n';
+ 
+typedef __gnu_pbds::tree<int, __gnu_pbds::null_type, less<int>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update> ordered_set;
+typedef long long ll;
+typedef set<ll> si;
+typedef set<char> sc;
+typedef vector<ll> vi;
+typedef priority_queue<ll> pri;
+typedef vector<char> vc;
+typedef pair<ll,ll> pi;
+typedef vector<pi>	vpi;
+typedef vector<vi>	vvi;
+typedef vector<vc>	vvc;
+typedef vector<string> vs;
+typedef vector<bool> vb;
+const long long M = 1e9+7;
+#define N  10000005
+#define lli unsigned long long int
+#define ll long long
+
+const int INF = INT_MIN;
+
+int somadigito(int num, int soma){ 
+    if(num==0) return soma; 
+    else return somadigito(num/10,soma+num%10);
+}
+
+bool fib(ll n) {
+    
+    if(n == 1 or n == 0) return true;
+    vector<int> arr(N);
+    arr[0] = 0;
+    arr[1] = 1;
+    arr[2] = 1;
+
+    forn{
+        if(i > 2)
+            arr[i] = arr[i-1] + arr[i-2];
+        if(arr[i] == n) return true;
+    }
+    return false;
+
+}
+ 
+int main () {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    ll t;
+    cin >> t;
+
+    bool ans = fib(somadigito(t,0));
+    printf("%s\n", ans ? "Sim" : "Nao");
+
+    return 0;
+
+}
