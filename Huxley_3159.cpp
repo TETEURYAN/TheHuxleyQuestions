4bc821b99692b73419731e664975f2e4a4691e04
Matheus Ryan <mrysn@ic.ufal.br>
2024-05-09 20:33:46 -0300

    3159 - Corona ep2

---
+++ Huxley_3159.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+ 
+using namespace std;
+ 
+const int N1 = (int)2e5 + 100;
+#define PI 3.14159265358979323846264338327
+#define forn for(int i=0;i<n;i++)
+#define fornn for(int i=1;i<=n;i++)
+#define each(i,c) for(__typeof((c).begin()) i=(c).begin(),i##_end=(c).end();i!=i##_end;++i)
+#define all(x) (x).begin(), (x).end()
+#define f first
+#define s second
+#define pb push_back
+#define pp pop_back
+#define mp make_pair
+#define yes cout<<"Yes"<<'\n';
+#define no  cout<<"No"<<'\n';
+#define one  cout<<-1<<'\n';
+#define nl  '\n';
+ 
+typedef __gnu_pbds::tree<int, __gnu_pbds::null_type, less<int>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update> ordered_set;
+typedef long long ll;
+typedef set<ll> si;
+typedef set<char> sc;
+typedef vector<ll> vi;
+typedef priority_queue<ll> pri;
+typedef vector<char> vc;
+typedef pair<ll,ll> pi;
+typedef vector<pi>	vpi;
+typedef vector<vi>	vvi;
+typedef vector<vc>	vvc;
+typedef vector<string> vs;
+typedef vector<bool> vb;
+const long long M = 1e9+7;
+#define N  10000005
+#define lli unsigned long long int
+#define ll long long
+
+const int INF = INT_MIN;
+
+vector<bool> is_prime(N+1, true);
+void sieve(){
+
+is_prime[0] = is_prime[1] = false;
+    for (int i = 2; i <= N; i++) {
+        if (is_prime[i] && (long long)i * i <= N) {
+            for (int j = i * i; j <= N; j += i)
+                is_prime[j] = false;
+        }
+    }
+}
+
+int main () {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+
+    sieve();
+
+    int ans = 0;
+    for(int i = 0; i < 3; i++){
+        int num;
+        cin >> num;
+        if(is_prime[num]) ans++;
+    }
+    printf("%s\n", (not ans) ? "Seguro" : ans == 1 ? "Poss�vel Risco" : "Perigo");
+
+    return 0;
+
+}
