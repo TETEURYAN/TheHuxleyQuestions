834894d9af36cb625cb5f350d5ffd77b4a676043
Matheus Ryan <mrysn@ic.ufal.br>
2024-05-30 23:53:49 -0300

    297 - Anos Bissextos

--- Huxley_0297.cpp
+++ Huxley_0297.cpp
@@ -1,9 +1,8 @@
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
- 
 using namespace std;
- 
+
 const int N1 = (int)2e5 + 100;
 #define PI 3.14159265358979323846264338327
 #define forn for(int i=0;i<n;i++)
