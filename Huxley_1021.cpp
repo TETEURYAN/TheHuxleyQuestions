5e47147a3d13e12538e03d5ccc38ed4afba1b2ed
Matheus Ryan <mrysn@ic.ufal.br>
2026-04-20 14:26:39 -0300

    1021 - Super Sales
    Implement a solution for a problem involving weights and values using dynamic programming.

---
+++ Huxley_1021.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+void solve(){
+    ll n;
+    cin >> n;
+
+    vector<ll> p(n), w(n);
+    for (ll i = 0; i < n; i++){
+        cin >> p[i] >> w[i];
+    }
+
+    vector<ll> dp(31, 0);
+
+    for (ll i = 0; i < n; i++){
+        for (ll j = 30; j >= w[i]; j--){
+            dp[j] = max(dp[j], dp[j - w[i]] + p[i]);
+        }
+    }
+
+    ll q;
+    cin >> q;
+
+    ll ans = 0;
+    while (q--){
+        ll x;
+        cin >> x;
+        ans += dp[x];
+    }
+
+    cout << ans << "\n";
+}
+
+signed main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    ll t;
+    cin >> t;
+    while (t--){
+        solve();
+    }
+
+    return 0;
+
+}
